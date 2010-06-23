e3b06ef9980a62e31d45b14099dced2e4f0fd730
Alan Gutierrez <alan@blogometer.com>
2010-06-23 13:31:42 -0500

    Javadoc typos.

--- src/main/java/com/goodworkalan/okay/ObjectValidator.java
+++ src/main/java/com/goodworkalan/okay/ObjectValidator.java
@@ -3,7 +3,7 @@ package com.goodworkalan.okay;
 /**
  * Validates an object recording errors in a collection of mistakes. This
  * is the common interface for all validation strategies. Validation strategies
- * are associated with a class in a {@link Validator].
+ * are associated with a class in a {@link Validator}.
  * 
  * @see Validator
  */
@@ -11,7 +11,7 @@ public interface ObjectValidator<T> {
     /**
      * Validate the given bean reporting errors in the given map of mistakes.
      * 
-     * @param bean
+     * @param object
      *            The object to validate.
      * @param mistakes
      *            The collection of mistakes.
